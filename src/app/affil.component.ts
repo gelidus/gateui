b59d6035d0d98710cbe1a17968c539349be2df60
Gelidus <gelidus@flowup.eu>
2016-05-20 16:28:02 +0200

    feature(auth): added roles, bizdev and partner menu placeholders and role observable

--- src/app/affil.component.ts
+++ src/app/affil.component.ts
@@ -4,6 +4,8 @@ import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
 import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
 import { MdButton } from '@angular2-material/button';
 
+import { AuthService } from './auth.service';
+
 @Component({
   moduleId: module.id,
   selector: 'affil-app',
@@ -24,4 +26,14 @@ export class AffilAppComponent {
   constructor() {
     
   }
+  
+  // menu that should be displayed to the business developers
+  bizdevMenu = [
+    
+  ];
+  
+  // menu that should be displayed to the partners
+  partnerMenu = [
+    
+  ];
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
+export enum Role {
+  Visitor,
+  Partner,
+  BusinessDeveloper
+}
 
 @Injectable()
 export class AuthService {
+  
+  public role: Subject<Role>;
 
-  constructor() { }
-
+  constructor() {
+    this.role = new Subject<Role>();
+  }
+  
+  updateRole(r: Role) {
+    this.role.next(r);
+  }
 }
