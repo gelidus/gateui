dcb0604f11a88e41deca6c5e97506beebc9c59d3
Gelidus <gelidus@flowup.eu>
2016-05-20 15:40:03 +0200

    feature(skeleton): added toolbar and sidenav

--- src/app/affil.component.ts
+++ src/app/affil.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+import { MdButton } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
   selector: 'affil-app',
   templateUrl: 'affil.component.html',
-  styleUrls: ['affil.component.css']
+  styleUrls: ['affil.component.css'],
+  directives: [
+    MdToolbar,
+    MdIcon,
+    MdButton,
+    MD_SIDENAV_DIRECTIVES
+  ],
+  providers: [
+    MdIconRegistry
+  ]
 })
 export class AffilAppComponent {
-  title = 'affil works!';
+  
+  constructor() {
+    
+  }
 }
